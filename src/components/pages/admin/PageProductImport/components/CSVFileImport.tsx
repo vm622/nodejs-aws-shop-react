9fe30c13b2545fb891541ed05a452696ef11ae00
vm622 <vm62285@gmail.com>
2024-07-01 13:53:31 +0300

    add products file load

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import axios from "axios";
 
 type CSVFileImportProps = {
   url: string;
@@ -25,22 +26,33 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const uploadFile = async () => {
     console.log("uploadFile to", url);
 
+    if (!file) {
+      alert("Please select a file");
+      return;
+    }
+
     // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
+    const response = await axios({
+      method: "GET",
+      url,
+      params: {
+        name: encodeURIComponent(file.name),
+      },
+    });
+    console.log("File to upload: ", file.name);
+
+    console.log("Uploading to: ", response.data.url);
+    console.log("FIle format: ", file.type)
+
+    const result = await fetch(response.data.url, {
+      method: "PUT",
+      body: file,
+      headers: {
+        'Content-Type': file.type
+      },
+    });
+    console.log("Result: ", result);
+    setFile(undefined);
   };
   return (
     <Box>
