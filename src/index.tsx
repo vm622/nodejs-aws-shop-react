3544b2e754d1811fde95d6eb34df12709c2f08b3
vm622 <vm62285@gmail.com>
2024-07-16 04:07:49 +0300

    add alerts for 403 and 401 errors

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from 'axios';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,21 @@ if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
 }
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.log(error)
+    const responseStatus = error.response.status;
+
+    if (responseStatus === 401) {
+      alert(`${responseStatus} Unauthorized`);
+    } else if (responseStatus === 403) {
+      alert(`${responseStatus} Access denied`);
+    }
+    return Promise.reject(error.response);
+  },
+);
 
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
